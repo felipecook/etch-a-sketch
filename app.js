f15952957965da38568fc99bf211014020c28055
felipe cook <felipepcook@gmail.com>
2022-03-30 09:40:43 -0400

    created drawScreen func and addEffects func, need to complete

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const container = document.querySelector(".container");
 console.log(container);
+
+
+
+
+// have this function draw a 4x4 grid of boxes.
+function drawScreen () {
+  container.appendChild()
+}
+
+
+function addEffects (e) {
+  e.classList.add('hovered');
+}
