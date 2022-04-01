87618866dda5c0b526ba3408b211f25bdbeccb41
felipe cook <felipepcook@gmail.com>
2022-03-31 22:09:40 -0400

    trying out new amount of squares, next step is user input

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ console.log(outerContainer);
 
 // have this function draw a 4x4 grid of boxes.
 function drawScreen() {
-  for (let index = 0; index < 4; index++) {
+  for (let index = 0; index < 16; index++) {
 
     //this creates the rows
     let innerContainer = document.createElement('div');
     innerContainer.classList.add('container');
     outerContainer.appendChild(innerContainer);
 
-    for (let innerIndex = 0; innerIndex < 4; innerIndex++) {
+    for (let innerIndex = 0; innerIndex < 16; innerIndex++) {
       //This creates the boxes
       let box = document.createElement('div');
       box.classList.add('box');
@@ -31,9 +31,9 @@ function addEffects(e) {
   console.log(e);
   e.target.classList.add('hovered')
 
-  setTimeout(function () {
-    e.target.classList.remove('hovered');
-  }, 500);
+  // setTimeout(function () {
+  //   e.target.classList.remove('hovered');
+  // }, 500);
 }
 
 drawScreen();
