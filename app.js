7ffc1a7bda28ff53989929dd8c2b95bcff208703
felipe cook <felipepcook@gmail.com>
2022-03-31 20:48:40 -0400

    first attempt at adding a column to each row. need to rethink

--- app.js
+++ app.js
@@ -8,23 +8,31 @@ console.log(container);
 // have this function draw a 4x4 grid of boxes.
 function drawScreen () {
   for (let index = 0; index < 4; index++) {
-    let singleDiv = document.createElement('div'); 
-    singleDiv.style.flex = '1';
-    singleDiv.style.width = '200px';
-    singleDiv.style.height = '200px';
-    singleDiv.style.background = 'lightgrey';
-    singleDiv.classList.add('etches')
-    container.appendChild(singleDiv);
-    console.log(index);
+
+    //This creates the row of divs, need a second for loop to create the columns at same length.
+    let rowDiv = document.createElement('div'); 
+    rowDiv.classList.add('row');
+    rowDiv.classList.add('etches');
+    container.appendChild(rowDiv);
+
+    for (let innerIndex = 0; innerIndex < 4; innerIndex++) {
+      let columnDiv = document.createElement('div');
+      columnDiv.classList.add('column');
+      columnDiv.classList.add('etches');
+      rowDiv.appendChild(columnDiv);
+    }
+    // console.log(index);
   }
   
 }
 
 
 function addEffects (e) {
-  // e.classList.add('hovered');
+  
   console.log(e);
   e.target.style.background = 'orange';
+  //attempting to use classList to add color and remove, but unsure why it is not working
+  e.target.classList.add('hovered')
 
   setTimeout(function() {
     e.target.style.background = 'lightgrey';
