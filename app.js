c5a02e156c9a3395a56d0fef85d5f1188b8ae650
felipe cook <felipepcook@gmail.com>
2022-03-31 21:08:50 -0400

    columns and rows appear, but hardset for 4x4, need to change

--- app.js
+++ app.js
@@ -1,40 +1,50 @@
-const container = document.querySelector(".container");
+const outerContainer = document.querySelector("#outerContainer");
 
-console.log(container);
+console.log(outerContainer);
 
 
 
 
 // have this function draw a 4x4 grid of boxes.
-function drawScreen () {
+function drawScreen() {
   for (let index = 0; index < 4; index++) {
 
-    //This creates the row of divs, need a second for loop to create the columns at same length.
-    let rowDiv = document.createElement('div'); 
-    rowDiv.classList.add('row');
-    rowDiv.classList.add('etches');
-    container.appendChild(rowDiv);
+    //this creates the rows
+    let innerContainer = document.createElement('div');
+    innerContainer.classList.add('container');
+    outerContainer.appendChild(innerContainer);
 
     for (let innerIndex = 0; innerIndex < 4; innerIndex++) {
+      //This creates the columns
       let columnDiv = document.createElement('div');
       columnDiv.classList.add('column');
       columnDiv.classList.add('etches');
-      rowDiv.appendChild(columnDiv);
+      innerContainer.appendChild(columnDiv);
+
     }
+
+
+
+    // for (let innerIndex = 0; innerIndex < 4; innerIndex++) {
+    //   let columnDiv = document.createElement('div');
+    //   columnDiv.classList.add('column');
+    //   columnDiv.classList.add('etches');
+    //   columnDiv.appendChild(columnDiv);
+    // }
     // console.log(index);
   }
-  
+
 }
 
 
-function addEffects (e) {
-  
+function addEffects(e) {
+
   console.log(e);
   e.target.style.background = 'orange';
   //attempting to use classList to add color and remove, but unsure why it is not working
-  e.target.classList.add('hovered')
+  // e.target.classList.add('hovered')
 
-  setTimeout(function() {
+  setTimeout(function () {
     e.target.style.background = 'lightgrey';
   }, 500);
 }
@@ -45,6 +55,6 @@ const divs = document.querySelectorAll('.etches');
 console.log(divs);
 
 divs.forEach(div => {
-console.log(div);
-div.addEventListener('mouseover', addEffects);
+  console.log(div);
+  div.addEventListener('mouseover', addEffects);
 });
