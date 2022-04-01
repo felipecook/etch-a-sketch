565a88acdc3e5436a531a36b3887f452edc68ae8
felipe cook <felipepcook@gmail.com>
2022-04-01 14:19:44 -0400

    draw screen works with button, but needs to remove hovered class upon pressing

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const outerContainer = document.querySelector("#outerContainer");
+let drawScreenButton = document.createElement('button');
+drawScreenButton.classList.add('drawScreenButton');
+drawScreenButton.innerText = 'Redraw Screen';
+outerContainer.appendChild(drawScreenButton);
 
 console.log(outerContainer);
 
 
 
 
-// have this function draw a 4x4 grid of boxes.
-function drawScreen() {
-  let drawScreenButton = document.createElement('button');
-  drawScreenButton.classList.add('drawScreenButton');
-  drawScreenButton.innerText = 'Redraw Screen';
-  outerContainer.appendChild(drawScreenButton);
-  for (let index = 0; index < 16; index++) {
+// have this function draw a user defined grid of boxes.
+function drawScreen(size) {
+
+
+  for (let index = 0; index < size; index++) {
+
+    const boxes = document.querySelectorAll('.box');
+    
+
+    boxes.forEach(box => {
+      console.log(box);
+      box.addEventListener('mouseover', addEffects);
+    });
 
     //this creates the rows
     let innerContainer = document.createElement('div');
     innerContainer.classList.add('container');
     outerContainer.appendChild(innerContainer);
 
-    for (let innerIndex = 0; innerIndex < 16; innerIndex++) {
+    for (let innerIndex = 0; innerIndex < size; innerIndex++) {
       //This creates the boxes
       let box = document.createElement('div');
       box.classList.add('box');
@@ -40,12 +50,22 @@ function addEffects(e) {
   // }, 500);
 }
 
-drawScreen();
+// drawScreen();
 
-const boxes = document.querySelectorAll('.box');
-console.log(boxes);
 
-boxes.forEach(box => {
-  console.log(box);
-  box.addEventListener('mouseover', addEffects);
+
+drawScreenButton.addEventListener('click', () => {
+
+  let size = parseInt(prompt('Please enter the size of the grid you want, must be a number', 4))
+
+  if (Number.isInteger(size) && size < 101){
+
+    drawScreen(size);
+  } else {
+    alert("You did not enter a number. Try again.")
+    return;
+  }
+
+  
+
 });
